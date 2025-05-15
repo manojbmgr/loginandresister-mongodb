d3bcf7463bb400dfb0c56a957de8cfb6314e0141
manojbmgr <43365915+manojbmgr@users.noreply.github.com>
2025-05-15 20:16:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('trust proxy', 1);
 console.log(`secret: ${process.env.SECRET}`); /// Debugging
 app.use(session({
   secret: process.env.SECRET,
