c986fe8a89aad1d15b99eb08200df75bfa9c48bc
manojbmgr <43365915+manojbmgr@users.noreply.github.com>
2025-05-15 19:17:38 +0530

    Update index.js
    debugging

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+console.log(`secret: ${process.env.secret}`); /// Debugging
 app.use(session({
   secret: process.env.SECRET,
   resave: false,
